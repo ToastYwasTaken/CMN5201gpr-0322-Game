265637738f47ee4c3ac4fdf8f35153175428db4b
Dereos <mario63db@gmail.com>
2022-05-23 22:24:56 +0200

    PauseUI fix

--- Assets/Scripts/UI/PauseUI.cs
+++ Assets/Scripts/UI/PauseUI.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using TMPro;
 
 namespace Dennis.UI
@@ -23,11 +22,9 @@ namespace Dennis.UI
 
         [Header("Misc")]
         [SerializeField]
-<<<<<<< Updated upstream
         private LoadingScreenUI loadingScreen;
-=======
+        [SerializeField]
         private TMP_Text versionText;
->>>>>>> Stashed changes
 
         private WindowController WindowController { get { return WindowController.s_Instance; } }
 
