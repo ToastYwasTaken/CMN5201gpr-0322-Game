7949259ef631b58b9c971fa2f0fe4b41566072b1
alsigmund <alsigmund@outlook.de>
2022-05-27 11:22:27 +0200

    Loottables
    Loottable function added
    Basic Enemy loottable added

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -6,6 +6,18 @@ public class Enemy : EntityStats
 {
     protected override void Death()
     {
+        DropLoot(_lootTable);
         Destroy(transform.parent.parent.parent.gameObject);
+
+    }
+
+    [SerializeField] private LootTable _lootTable;
+
+    protected virtual void DropLoot(LootTable lootTable)
+    {
+        if (_lootTable == null) return;
+
+        GameObject droppedLoot = _lootTable.DetermineLoot();
+        if (droppedLoot != null) Instantiate(droppedLoot, transform.position, Quaternion.identity);
     }
 }
