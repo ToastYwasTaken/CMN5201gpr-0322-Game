681f672c50253f5c6352f6b16adf22a062009603
Dereos <mario63db@gmail.com>
2022-05-30 19:17:23 +0200

    MainMenu Animations

--- Assets/Scripts/UI/ParallaxUI.cs
+++ Assets/Scripts/UI/ParallaxUI.cs
@@ -6,6 +6,8 @@ namespace Dennis.UI
     {
         [SerializeField]
         private float moveModifier;
+        [SerializeField]
+        private float time;
 
         private Vector2 startPosition;
 
@@ -18,8 +20,8 @@ namespace Dennis.UI
         {
             Vector2 position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-            float positionX = Mathf.Lerp(transform.position.x, startPosition.x + (position.x * moveModifier), 2f * Time.deltaTime);
-            float positionY = Mathf.Lerp(transform.position.y, startPosition.y + (position.y * moveModifier), 2f * Time.deltaTime);
+            float positionX = Mathf.Lerp(transform.position.x, startPosition.x + (position.x * moveModifier), time * Time.deltaTime);
+            float positionY = Mathf.Lerp(transform.position.y, startPosition.y + (position.y * moveModifier), time * Time.deltaTime);
 
             transform.position = new Vector2(positionX, positionY);
         }
