22b1c4b90cac6c03ac7f26d4561f54f514d84879
ToastYwasTaken <franz@emoerike.de>
2022-06-01 13:53:25 +0200

    added music volume fading

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -18,6 +18,7 @@ public class AudioManager : MonoBehaviour
     public AudioClip MusicBossRoom;
 
     private static AudioManager s_instance;
+    private AudioMixer _audioMixer;
     void Awake()
     {
         if (s_instance != null && s_instance != this)
@@ -29,6 +30,10 @@ public class AudioManager : MonoBehaviour
             s_instance = this;
         }
         DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+       _audioMixer = FindObjectOfType<AudioMixer>();
     }
 
     /// <summary>
@@ -51,11 +56,21 @@ public class AudioManager : MonoBehaviour
         _musicSource.UnPause();
     }
 
+    /// <summary>
+    ///Fades out old melody, fades in new melody
+    ///unfortunately can't do simultaneously
+    /// </summary>
+    /// <param name="newMelodyClip"></param>
     public void ChangeMelody(AudioClip newMelodyClip)
     {
+        float originalVolume = _musicSource.volume;
+        float fadedVolume = originalVolume / 8;
+        StartCoroutine(Fade(2, fadedVolume));
         _musicSource.Stop();
         _musicSource.clip = newMelodyClip;
+        _musicSource.volume = fadedVolume;
         _musicSource.Play();
+        StartCoroutine(Fade(2, originalVolume));
     }
 
     /// <summary>
@@ -67,4 +82,21 @@ public class AudioManager : MonoBehaviour
         _soundSource.clip = soundClip;
         _soundSource.Play();
     }
+
+    private IEnumerator Fade(float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float startVolume = _musicSource.volume;
+        while(currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            Debug.Log("Volume: " + _musicSource.volume);
+            _musicSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
+        yield return null;
+        }
+        yield break;
+    }
+   
+
+
 }
