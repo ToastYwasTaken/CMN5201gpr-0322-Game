2a0c526e4d1c81194d88ee866b008c1ac1c4dce2
Dereos <mario63db@gmail.com>
2022-06-03 13:15:58 +0200

    Victory + Defeat Window

---
+++ Assets/Scripts/UI/DefeatWindowUI.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dennis.UI
+{
+    public class DefeatWindowUI : MonoBehaviour
+    {
+        [Header("Window")]
+        [SerializeField]
+        private GameObject DefeatWindow;
+
+        [Header("Buttons")]
+        [SerializeField]
+        private Button RestartButton;
+        [SerializeField]
+        private Button ExitButton;
+
+        [Header("Misc")]
+        [SerializeField]
+        private LoadingScreenUI loadingScreen;
+
+        private void Start()
+        {
+            RestartButton.onClick.AddListener(Restart);
+            ExitButton.onClick.AddListener(ExitToMainMenu);
+        }
+
+        /// <summary>
+        /// Show defeat window.
+        /// </summary>
+        public void ShowDefeatWindow()
+        {
+            SetTimeScale(0f);
+
+            GlobalValues.sCurrentLevel++;
+            GlobalValues.sIsPlayerActive = false;
+
+            DefeatWindow.SetActive(true);
+        }
+
+        /// <summary>
+        /// Set the time scale.
+        /// </summary>
+        void SetTimeScale(float scale)
+        {
+            Time.timeScale = scale;
+        }
+
+        /// <summary>
+        /// Restart the current scene.
+        /// </summary>
+        void Restart()
+        {
+            SetTimeScale(1f);
+
+            loadingScreen.LoadScene(1);
+        }
+
+        /// <summary>
+        /// Exit to MainMenu scene.
+        /// </summary>
+        void ExitToMainMenu()
+        {
+            SetTimeScale(1f);
+
+            loadingScreen.LoadScene(0);
+        }
+    }
+}
+
