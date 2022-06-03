9e30317059c93456a167c782d0812d688a542858
realcat-c <88097738+realcat-c@users.noreply.github.com>
2022-06-03 14:15:10 +0200

    lvlgen bug

--- Assets/Scripts/Behaviour/Level/Doors/DoorManager.cs
+++ Assets/Scripts/Behaviour/Level/Doors/DoorManager.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
-    [SerializeField] Transform _map;
-    Transform _hallways;
-    Dictionary<Vector2, IDoor> _doorsDic = new Dictionary<Vector2, IDoor>();
+    [SerializeField] GameObject _doorFinderGO;
     List<IDoor>[] _doorsByRoom;
+    DoorFinder _doorFinder;
+
+    private void Awake()
+    {
+        RefLib.sDoorManager = this;
+        
+    }
 
     private void Start()
     {
-        if (_map == null) return;
-        GetHallWays();
-        GetDoors();
+        _doorFinder = _doorFinderGO.GetComponent<DoorFinder>();
+        FindDoors();
+        OpenDoors(0);
     }
 
     public void OpenDoors(int room)
@@ -22,48 +27,75 @@ public class DoorManager : MonoBehaviour
         foreach(IDoor door in _doorsByRoom[room])
             door.UnlockDoor();
     }
-    void GetHallWays()
+
+    public void AddDoor(int room, IDoor door)
     {
-        foreach (Transform child in _map.transform)
-            if (child.transform.name.Contains("Hallways"))
-                _hallways = child;
+        _doorsByRoom[room].Add(door);
     }
-    void GetDoors()
+
+    void FindDoors()
     {
-        List<GameObject> doors = new List<GameObject>();
-        foreach (Transform child in _hallways)
-            if (child.transform.CompareTag("Door"))
-                doors.Add(child.gameObject);
-
-
-        foreach (RoomDoorConnection doorsR in BSPMap.s_doorsBetweenRooms)
-        {
-            string door1 = ((int)doorsR.DoorRoom1.x).ToString() + "|" +  ((int)doorsR.DoorRoom1.y).ToString();
-            string door2 = ((int)doorsR.DoorRoom2.x).ToString() + "|" +  ((int)doorsR.DoorRoom2.y).ToString(); /////
-
-            foreach (GameObject door in doors)
-            {
-                if (door.name.Contains(door1))
-                    _doorsDic.Add(doorsR.DoorRoom1, door.GetComponent<IDoor>());
-                else if (door.name.Contains(door2))
-                    _doorsDic.Add(doorsR.DoorRoom2, door.GetComponent<IDoor>());
-            }
-        }
         List<Room> rooms = BSPMap.s_allRooms;
         _doorsByRoom = new List<IDoor>[rooms.Count];
         for (int i = 0; i < rooms.Count; i++)
             foreach (RoomDoorConnection doorsR in BSPMap.s_doorsBetweenRooms)
             {
+                _doorFinder.Room1 = i;
+                _doorFinder.IsActive = true;
                 if (doorsR.Room1.X == rooms[i].X && doorsR.Room1.Y == rooms[i].Y)
                 {
-                    _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom1]);
-                    _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom2]);
+                    _doorFinderGO.transform.position = new Vector3(doorsR.DoorRoom1.x, doorsR.DoorRoom1.y, 0);
+                    _doorFinderGO.transform.position = new Vector3(doorsR.DoorRoom2.x, doorsR.DoorRoom2.y, 0);
                 }
                 if (doorsR.Room2.X == rooms[i].X && doorsR.Room2.Y == rooms[i].Y)
                 {
-                    _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom1]);
-                    _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom2]);
+                    _doorFinderGO.transform.position = new Vector3(doorsR.DoorRoom1.x, doorsR.DoorRoom1.y, 0);
+                    _doorFinderGO.transform.position = new Vector3(doorsR.DoorRoom2.x, doorsR.DoorRoom2.y, 0);
                 }
             }
     }
+    //void GetHallWays()
+    //{
+    //    foreach (Transform child in _map.transform)
+    //        if (child.transform.name.Contains("Hallways"))
+    //            _hallways = child;
+    //}
+    //void GetDoors()
+    //{
+    //    List<GameObject> doors = new List<GameObject>();
+    //    foreach (Transform child in _hallways)
+    //        if (child.transform.CompareTag("Door"))
+    //            doors.Add(child.gameObject);
+
+
+    //    foreach (RoomDoorConnection doorsR in BSPMap.s_doorsBetweenRooms)
+    //    {
+    //        string door1 = ((int)doorsR.DoorRoom1.x).ToString() + "|" +  ((int)doorsR.DoorRoom1.y).ToString();
+    //        string door2 = ((int)doorsR.DoorRoom2.x).ToString() + "|" +  ((int)doorsR.DoorRoom2.y).ToString(); /////
+
+    //        foreach (GameObject door in doors)
+    //        {
+    //            if (door.name.Contains(door1))
+    //                _doorsDic.Add(doorsR.DoorRoom1, door.GetComponent<IDoor>());
+    //            else if (door.name.Contains(door2))
+    //                _doorsDic.Add(doorsR.DoorRoom2, door.GetComponent<IDoor>());
+    //        }
+    //    }
+    //    List<Room> rooms = BSPMap.s_allRooms;
+    //    _doorsByRoom = new List<IDoor>[rooms.Count];
+    //    for (int i = 0; i < rooms.Count; i++)
+    //        foreach (RoomDoorConnection doorsR in BSPMap.s_doorsBetweenRooms)
+    //        {
+    //            if (doorsR.Room1.X == rooms[i].X && doorsR.Room1.Y == rooms[i].Y)
+    //            {
+    //                _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom1]);
+    //                _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom2]);
+    //            }
+    //            if (doorsR.Room2.X == rooms[i].X && doorsR.Room2.Y == rooms[i].Y)
+    //            {
+    //                _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom1]);
+    //                _doorsByRoom[i].Add(_doorsDic[doorsR.DoorRoom2]);
+    //            }
+    //        }
+    //}
 }
